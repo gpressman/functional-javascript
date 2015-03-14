a215bddb0680c64a4dda9dd4374b4d162788be9c
Gabe Pressman <pressman4747@gmail.com>
2015-03-13 20:06:17 -0400

    first functional commit

---
+++ higher-order-functions.js
@@ -0,0 +1,12 @@
+function repeat(operation, num) {
+      for (var i = 0; i<num; i++) {
+      	return function operation()
+      	
+      }
+  })
+      	
+      
+   
+
+    // Do not remove the line below
+    module.exports = repeat
